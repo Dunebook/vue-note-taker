7e2ffe1ff7d231ee0763371a7c58c6f47c67f7ef
Olowu Emmanuel <emmanuelr20@yahoo.com>
2018-12-08 12:42:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <span class="title is-size-1">NoteTaker</span>
+          </a>
+        </div>
+
+      </div>
+    </nav>
+    <br><br>
+    <div  class="container">
+      <NoteList />
+    </div>
+  </div>
+</template>
+
+<script>
+import NoteList from './components/NoteList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NoteList
+  }
+}
+</script>
